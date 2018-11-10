40786acc011e334eb19eb2863e008d89ab8fcc29
Jbrown95 <browningjustin95@gmail.com>
2018-11-10 01:45:09 -0500

    tested if board array equals 35 failed

--- board_test.rb
+++ board_test.rb
@@ -6,4 +6,8 @@ class TestUntitled < Test::Unit::TestCase
     def test_if_1_equals_1
         assert_equal 1, 1
     end
+
+    def test_if_board_array_length_is_35
+        assert_equal board.b.length, 35
+    end
 end
