3bc2b690180f4b797b6aee2e05a10a72e2788c83
Jbrown95 <browningjustin95@gmail.com>
2018-11-29 16:53:17 -0500

    created a function to draw the board in console

--- board.rb
+++ board.rb
@@ -6,4 +6,13 @@ class Board
     def initialize
         @b = [*1..42]
     end
+
+    def draw_board
+        @b.each_with_index do |x,i| 
+			print "| #{x} |" unless (i + 1) % 7 == 0
+			if (i + 1) % 7 == 0
+			print	x = "| #{x} |\n\n"
+			end
+		end
+    end
 end
