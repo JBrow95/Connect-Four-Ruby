f204ebe5ad0476cec1e28d74460dd182353991ed
Jbrown95 <browningjustin95@gmail.com>
2018-11-29 17:04:14 -0500

    created class for the game, and created a function to start the game by drawing the board

---
+++ game.rb
@@ -0,0 +1,14 @@
+require_relative 'board.rb'
+
+class Game
+    def initialize
+        @game_baord = Board.new
+    end
+
+    def play
+        @game_baord.draw_board
+    end
+end
+
+c4 = Game.new
+c4.play
