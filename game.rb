f87c89f6177e44c7f7680e4fcded9d8f32fa75fe
Jbrown95 <browningjustin95@gmail.com>
2018-11-29 19:33:35 -0500

    created file with player class to return names

--- game.rb
+++ game.rb
@@ -1,10 +1,17 @@
 require_relative 'board.rb'
+require_relative 'player.rb'
 
 class Game
     def initialize
         @game_baord = Board.new
+        @player1 = Player.new(get_names(1), "B")
+        @player2 = Player.new(get_names(2), "R")
     end
 
+    def get_names(x)
+        puts "\nPlayer #{x} enter your name: "
+        gets.chomp
+    end
 
     def play
         @game_baord.draw_board
